4d97f27abde52f1b54c7a3fab6efe8506ace86cb
Mark McGranaghan <mmcgrana@gmail.com>
2011-01-25 20:34:11 -0800

    pipe and cloud spikes

---
+++ src/pulse/pipe.clj
@@ -0,0 +1,12 @@
+(ns pulse.pipe
+  (:import (java.io )))
+
+(defn pipe-lines [f]
+  (loop []
+    (when-let [line (.readLine *in*)]
+      (f line)
+      (recur))))
+
+(pipe-lines
+  (fn [line]
+    ()))
