e9909d38bb34ec2eed1d4ec3e4cbe9dd107a3383
Mark McGranaghan <mmcgrana@gmail.com>
2011-01-25 19:57:29 -0800

    dynamic type

--- src/pulse/spike.clj
+++ src/pulse/spike.clj
@@ -4,16 +4,11 @@
 
 (set! *warn-on-reflection* true)
 
-(let [evt-props (doto (Properties.)
-                  (.put "price" "double")
-                  (.put "timestamp" "long")
-                  (.put "symbol" "string"))
-      config (doto (Configuration.)
-               (.addEventType "tickEvent" evt-props))
+(let [config (doto (Configuration.) (.addEventType "devent" (Properties.)))
       service (EPServiceProviderManager/getDefaultProvider config)
       runtime (.getEPRuntime service)
       admin (.getEPAdministrator service)
-      statement (.createEPL admin "select * from tickEvent(symbol='APPL').win:length(2) having avg(price) > 6.0")
+      statement (.createEPL admin "select * from devent(symbol?='APPL').win:length(2) having avg(cast(price?,double)) > 6.0")
       listener (proxy [UpdateListener] []
                  (update [new-evts _]
                    (println "update" (.getUnderlying ^EventBean (first new-evts)))))]
@@ -24,4 +19,4 @@
           symbol "APPL"
           tick {"price" price "timestamp" timestamp "symbol" symbol}]
       (println "send" tick)
-      (.sendEvent runtime tick "tickEvent"))))
+      (.sendEvent runtime tick "devent"))))
