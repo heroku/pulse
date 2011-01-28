fc14810a58c06ae9fcc27fc1b7663bfebea94da0
Mark McGranaghan <mmcgrana@gmail.com>
2011-01-28 06:44:02 -0800

    shell-lines

--- src/pulse/pipe.clj
+++ src/pulse/pipe.clj
@@ -1,5 +1,6 @@
 (ns pulse.pipe
-  (:import clojure.lang.LineNumberingPushbackReader))
+  (:import clojure.lang.LineNumberingPushbackReader)
+  (:import (java.io InputStreamReader BufferedReader)))
 
 (set! *warn-on-reflection* true)
 
@@ -8,3 +9,12 @@
     (when-let [line (.readLine ^LineNumberingPushbackReader *in*)]
       (handler line)
       (recur))))
+
+(defn shell-lines [cmd-list handler]
+  (let [rt (Runtime/getRuntime)
+       proc (.exec rt ^"[Ljava.lang.String;" (into-array cmd-list))]
+    (with-open [out (-> (.getInputStream proc) (InputStreamReader.) (BufferedReader.))]
+      (loop []
+        (when-let [line (.readLine out)]
+          (handler line)
+          (recur))))))
