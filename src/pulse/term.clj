b8a788d5a9b930dabeb2abc5449c004452338335
Mark McGranaghan <mmcgrana@gmail.com>
2011-01-28 06:30:08 -0800

    whitespace

--- src/pulse/term.clj
+++ src/pulse/term.clj
@@ -57,65 +57,158 @@
 
 (defn -main []
   (let [service (engine/init-service)]
-    (engine/add-query service "select count(*) from devent.win:time(10 sec) output every 1 second"
+    (engine/add-query service
+      "select count(*) from devent.win:time(10 sec)
+         output every 1 second"
       (fn [[evt] _]
         (publish "events_per_second" (long (/ (get evt "count(*)") 10.0)))))
-    (engine/add-query service "select count(*) from devent.win:time(10 sec) where ((event_type? = 'nginx_access') and (http_host? != '127.0.0.1')) output every 1 second"
+
+    (engine/add-query service
+      "select count(*) from devent.win:time(10 sec)
+         where ((event_type? = 'nginx_access') and (http_host? != '127.0.0.1'))
+         output every 1 second"
       (fn [[evt] _]
         (publish "nginx_requests_per_second" (long (/ (get evt "count(*)") 10.0)))))
-    (engine/add-query service "select http_domain?, count(*) from devent.win:time(10 sec) where ((event_type? = 'nginx_access') and (http_host? != '127.0.0.1')) group by http_domain? output snapshot every 1 second order by count(*) desc limit 5"
+
+    (engine/add-query service
+      "select http_domain?, count(*) from devent.win:time(10 sec)
+         where ((event_type? = 'nginx_access') and (http_host? != '127.0.0.1'))
+         group by http_domain?
+         output snapshot every 1 second
+         order by count(*) desc
+         limit 5"
       (fn [evts _]
-        (publish "nginx_requests_per_second_top_domains" (map (fn [evt] [(get evt "http_domain?") (long (/ (get evt "count(*)") 10.0))]) evts))))
-    (engine/add-query service "select http_domain?, count(*) from devent.win:time(10 sec) where ((event_type? = 'nginx_access') and (http_host? != '127.0.0.1') and (cast(http_status?,long) >= 500)) group by http_domain? output snapshot every 1 second order by count(*) desc limit 5"
+        (publish "nginx_requests_per_second_top_domains"
+          (map (fn [evt] [(get evt "http_domain?") (long (/ (get evt "count(*)") 10.0))]) evts))))
+
+    (engine/add-query service
+      "select http_domain?, count(*) from devent.win:time(10 sec)
+         where ((event_type? = 'nginx_access') and
+                (http_host? != '127.0.0.1') and
+                (cast(http_status?,long) >= 500))
+         group by http_domain?
+         output snapshot every 1 second
+         order by count(*) desc
+         limit 5"
       (fn [evts _]
-        (publish "nginx_errors_per_minute_top_domains" (map (fn [evt] [(get evt "http_domain?") (get evt "count(*)")]) evts))))
-    (engine/add-query service "select count(*) from devent.win:time(60 sec) where (event_type? = 'nginx_error') output every 1 second"
+        (publish "nginx_errors_per_minute_top_domains"
+          (map (fn [evt] [(get evt "http_domain?") (get evt "count(*)")]) evts))))
+
+    (engine/add-query service
+      "select count(*) from devent.win:time(60 sec)
+         where (event_type? = 'nginx_error')
+         output every 1 second"
       (fn [[evt] _]
         (publish "nginx_errors_per_minute" (get evt "count(*)"))))
+
     (doseq [s ["500" "502" "503" "504"]]
-      (engine/add-query service (str "select count(*) from devent.win:time(60 sec) where ((event_type? = 'nginx_access') and (cast(http_status?,long) = " s ")) output every 1 second")
+      (engine/add-query service
+        (str "select count(*) from devent.win:time(60 sec)
+                where ((event_type? = 'nginx_access') and
+                       (cast(http_status?,long) = " s "))
+                output every 1 second")
         (fn [[evt] _]
           (publish (str "nginx_" s "_per_minute") (get evt "count(*)")))))
-    (engine/add-query service (str "select count(*) from devent.win:time(10 sec) where ((event_type? = 'hermes') and exists(domain?)) output every 1 second")
-        (fn [[evt] _]
-          (publish (str "hermes_proxies_per_second") (long (/ (get evt "count(*)") 10.0)))))
+
+    (engine/add-query service
+      (str "select count(*) from devent.win:time(10 sec)
+              where ((event_type? = 'hermes') and exists(domain?))
+              output every 1 second")
+      (fn [[evt] _]
+        (publish "hermes_proxies_per_second" (long (/ (get evt "count(*)") 10.0)))))
+
     (doseq [e ["H10" "H11" "H12" "H13" "H99"]]
-      (engine/add-query service (str "select count(*) from devent.win:time(60 sec) where ((event_type? = 'hermes') and (Error? = true) and ("e "? = true)) output every 1 second")
+      (engine/add-query service
+        (str "select count(*) from devent.win:time(60 sec)
+                where ((event_type? = 'hermes') and
+                       (Error? = true) and
+                       ("e "? = true))
+                output every 1 second")
         (fn [[evt] _]
           (publish (str "hermes_" e "_per_minute") (get evt "count(*)")))))
-    (engine/add-query service (str "select count(*) from devent.win:time(10 sec) where (converge_service? = true) output every 1 second")
-        (fn [[evt] _]
-          (publish (str "ps_converges_per_second") (long (/ (get evt "count(*)") 10.0)))))
-    (engine/add-query service "select count(*) from devent.win:time(60 sec) where ((amqp_publish? = true) and (cast(exchange?,string) regexp '(ps\\.run|service\\.needed).*')) output every 1 second"
-        (fn [[evt] _]
-          (publish "ps_run_requests_per_minute" (get evt "count(*)"))))
-    (engine/add-query service "select count(*) from devent.win:time(60 sec) where ((amqp_publish? = true) and (cast(exchange?,string) regexp 'ps\\.kill.*')) output every 1 second"
-        (fn [[evt] _]
-          (publish "ps_stop_requests_per_minute" (get evt "count(*)"))))
-    (engine/add-query service "select count(*) from devent.win:time(60 sec) where ((railgun_service? = true) and (ps_kill? = true) and (reason? = 'load')) output every 1 second"
-        (fn [[evt] _]
-          (publish "ps_kill_requests_per_minute" (get evt "count(*)"))))
-    (engine/add-query service "select count(*) from devent.win:time(60 sec) where ((railgun_ps_watch? = true) and (invoke_ps_run? = true)) output every 1 second"
-        (fn [[evt] _]
-          (publish "ps_runs_per_minute" (get evt "count(*)"))))
-    (engine/add-query service "select count(*) from devent.win:time(60 sec) where ((railgun_ps_watch? = true) and (handle_ps_return? = true)) output every 1 second"
+
+    (engine/add-query service
+      (str "select count(*) from devent.win:time(10 sec)
+              where (converge_service? = true)
+              output every 1 second")
+      (fn [[evt] _]
+        (publish "ps_converges_per_second" (long (/ (get evt "count(*)") 10.0)))))
+
+    (engine/add-query service
+      "select count(*) from devent.win:time(60 sec)
+         where ((amqp_publish? = true) and
+                (cast(exchange?,string) regexp '(ps\\.run|service\\.needed).*'))
+         output every 1 second"
+      (fn [[evt] _]
+        (publish "ps_run_requests_per_minute" (get evt "count(*)"))))
+
+    (engine/add-query service
+      "select count(*) from devent.win:time(60 sec)
+         where ((amqp_publish? = true) and
+                (cast(exchange?,string) regexp 'ps\\.kill.*'))
+         output every 1 second"
+      (fn [[evt] _]
+        (publish "ps_stop_requests_per_minute" (get evt "count(*)"))))
+
+    (engine/add-query service
+      "select count(*) from devent.win:time(60 sec)
+         where ((railgun_service? = true) and
+                (ps_kill? = true) and
+                (reason? = 'load'))
+         output every 1 second"
+      (fn [[evt] _]
+        (publish "ps_kill_requests_per_minute" (get evt "count(*)"))))
+
+    (engine/add-query service
+      "select count(*) from devent.win:time(60 sec)
+         where ((railgun_ps_watch? = true) and (invoke_ps_run? = true))
+         output every 1 second"
+      (fn [[evt] _]
+        (publish "ps_runs_per_minute" (get evt "count(*)"))))
+
+    (engine/add-query service
+      "select count(*) from devent.win:time(60 sec)
+         where ((railgun_ps_watch? = true) and (handle_ps_return? = true))
+         output every 1 second"
         (fn [[evt] _]
           (publish "ps_returns_per_minute" (get evt "count(*)"))))
-    (engine/add-query service "select count(*) from devent.win:time(60 sec) where ((railgun_ps_watch? = true) and (trap_exit? = true)) output every 1 second"
-        (fn [[evt] _]
-          (publish "ps_traps_per_minute" (get evt "count(*)"))))
-    (engine/add-query service (str "select cast(lastever(total_count?),long) as count from devent where (process_lost? = true) output first every 1 second")
+
+    (engine/add-query service
+      "select count(*) from devent.win:time(60 sec) where
+         ((railgun_ps_watch? = true) and (trap_exit? = true))
+         output every 1 second"
+      (fn [[evt] _]
+        (publish "ps_traps_per_minute" (get evt "count(*)"))))
+
+    (engine/add-query service
+      (str "select cast(lastever(total_count?),long) as count from devent
+              where (process_lost? = true)
+              output first every 1 second")
         (fn [[evt] _]
           (publish "ps_lost" (get evt "count"))))
+
     (doseq [[k p] {"invokes" "(invoke? = true)"
                    "fails"   "((compile_error? = true) or (locked_error? = true))"
                    "errors"  "((publish_error? = true) or (unexpected_error? = true))"}]
-      (engine/add-query service (str "select count(*) from devent.win:time(60 sec) where ((slugc_bin? = true) and " p ") output every 1 second")
+      (engine/add-query service
+        (str "select count(*) from devent.win:time(60 sec)
+                where ((slugc_bin? = true) and
+                       " p ")
+                output every 1 second")
         (fn [[evt] _]
           (publish (str "slugc_" k "_per_minute") (get evt "count(*)")))))
-    (engine/add-query service "select exchange?, count(*) from devent.win:time(60 sec) where (amqp_publish? = true) group by exchange? output snapshot every 1 second order by count(*) desc limit 5"
+
+    (engine/add-query service
+      "select exchange?, count(*) from devent.win:time(60 sec)
+         where (amqp_publish? = true)
+         group by exchange?
+         output snapshot every 1 second
+         order by count(*) desc
+         limit 5"
       (fn [evts _]
-        (publish "amqp_publishes_per_minute_top_exchanges" (map (fn [evt] [(get evt "exchange?") (get evt "count(*)")]) evts))))
+        (publish "amqp_publishes_per_minute_top_exchanges"
+          (map (fn [evt] [(get evt "exchange?") (get evt "count(*)")]) evts))))
+
     (pipe/stdin-lines
       (fn [line]
         (if-let [evt (parse/parse-line line)]
