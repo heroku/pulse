9f0fa111bf64ecf59070647649370cfa0b2d24ad
Mark McGranaghan <mmcgrana@gmail.com>
2011-02-05 18:42:30 -0800

    or redistogo

--- src/pulse/conf.clj
+++ src/pulse/conf.clj
@@ -3,6 +3,6 @@
 
 (def env (System/getenv))
 (def port (Integer/parseInt (get env "PORT" "8080")))
-(def redis-url (get env "REDIS_URL"))
+(def redis-url (or (get env "REDIS_URL") (get env "REDISTOGO_URL")))
 (def forwarder-hosts (if-let [h (get env "FORWARDER_HOSTS")] (str/split h #",")))
 (def logplex-hosts (if-let [h (get env "LOGPLEX_HOSTS")] (str/split h #",")))
