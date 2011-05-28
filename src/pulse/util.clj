356c9049434f0d0dfe47433e5eb1b1ee9a1de012
Mark McGranaghan <mmcgrana@gmail.com>
2011-05-28 09:01:01 -0700

    crashing

--- src/pulse/util.clj
+++ src/pulse/util.clj
@@ -4,8 +4,16 @@
 
 (set! *warn-on-reflection* true)
 
+(defn ^Runnable crashing [f]
+  (fn []
+    (try
+      (f)
+      (catch Exception e
+        (.printStackTrace e)
+        (System/exit 1)))))
+
 (defn spawn [f]
-  (let [t (Thread. ^Runnable f)]
+  (let [t (Thread. (crashing f))]
     (.start t)
     t))
 
@@ -18,7 +26,7 @@
 
 (defn spawn-tick [t f]
   (let [e (Executors/newSingleThreadScheduledExecutor)]
-    (.scheduleAtFixedRate e ^Runnable f 0 t TimeUnit/MILLISECONDS)))
+    (.scheduleAtFixedRate e (crashing f) 0 t TimeUnit/MILLISECONDS)))
 
 (defn log [fmt & args]
   (locking *out*
