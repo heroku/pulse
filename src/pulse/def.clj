f2046bbe570175006f013704cfe8d7ec1ffb7947
Mark McGranaghan <mmcgrana@gmail.com>
2011-05-28 18:40:46 -0700

    abstract

--- src/pulse/def.clj
+++ src/pulse/def.clj
@@ -8,16 +8,13 @@
 (defn safe-inc [n]
   (inc (or n 0)))
 
-(defn heroku? [evt]
-  (= (:cloud evt) "heroku.com"))
-
-(def events
+(defn rate [pred-fn]
   {:receive-init
      (fn []
        [(util/millis) 0])
    :receive-apply
      (fn [[window-start window-count] event]
-       [window-start (inc window-count)])
+       [window-start (if (pred-fn event) (inc window-count) window-count)])
    :receive-emit
      (fn [[window-start window-count]]
        [window-start (util/millis) window-count])
@@ -36,94 +33,13 @@
              complete-rate (/ complete-count 10.0)]
          [recent-windows complete-rate]))})
 
-(def events-per-second-by-parsed
+(defn rate-by-key [pred-fn key-fn]
   {:receive-init
      (fn []
        [(util/millis) {}])
    :receive-apply
      (fn [[window-start window-counts] event]
-       [window-start (update window-counts (str (:parsed event)) safe-inc) window-counts])
-   :receive-emit
-     (fn [[window-start window-counts]]
-       [window-start (util/millis) window-counts])
-   :merge-init
-     (fn []
-       [])
-   :merge-apply
-     (fn [windows window]
-       (conj windows window))
-   :merge-emit
-     (fn [windows]
-        (let [now (util/millis)
-              recent-windows (filter (fn [[window-start _ _]] (>= window-start (- now 11000))) windows)
-              complete-windows (filter (fn [[window-start _ _]] (< window-start (- now 1000))) recent-windows)
-              complete-counts (apply merge-with + (map (fn [[_ _ window-counts]] window-counts) complete-windows))
-              complete-sorted-counts (sort-by (fn [[k kc]] (- kc)) complete-counts)
-              complete-high-counts (take 10 complete-sorted-counts)
-              complete-rates (map (fn [[k kc]] [k (/ kc 10.0)]) complete-high-counts)]
-          [recent-windows complete-rates]))})
-
-(def events-per-second-by-aorta-host
-  {:receive-init
-     (fn []
-       [(util/millis) {}])
-   :receive-apply
-     (fn [[window-start window-counts] event]
-       [window-start (update window-counts (:aorta_host event) safe-inc)])
-   :receive-emit
-     (fn [[window-start window-counts]]
-       [window-start (util/millis) window-counts])
-   :merge-init
-     (fn []
-       [])
-   :merge-apply
-     (fn [windows window]
-       (conj windows window))
-   :merge-emit
-     (fn [windows]
-        (let [now (util/millis)
-              recent-windows (filter (fn [[window-start _ _]] (>= window-start (- now 11000))) windows)
-              complete-windows (filter (fn [[window-start _ _]] (< window-start (- now 1000))) recent-windows)
-              complete-counts (apply merge-with + (map (fn [[_ _ window-counts]] window-counts) complete-windows))
-              complete-sorted-counts (sort-by (fn [[k kc]] (- kc)) complete-counts)
-              complete-high-counts (take 10 complete-sorted-counts)
-              complete-rates (map (fn [[k kc]] [k (/ kc 10.0)]) complete-high-counts)]
-          [recent-windows complete-rates]))})
-
-(def events-per-second-by-event-type
-  {:receive-init
-     (fn []
-       [(util/millis) {}])
-   :receive-apply
-     (fn [[window-start window-counts] event]
-       [window-start (update window-counts (or (:event_type event) "none") safe-inc)])
-   :receive-emit
-     (fn [[window-start window-counts]]
-       [window-start (util/millis) window-counts])
-   :merge-init
-     (fn []
-       [])
-   :merge-apply
-     (fn [windows window]
-       (conj windows window))
-   :merge-emit
-     (fn [windows]
-        (let [now (util/millis)
-              recent-windows (filter (fn [[window-start _ _]] (>= window-start (- now 11000))) windows)
-              complete-windows (filter (fn [[window-start _ _]] (< window-start (- now 1000))) recent-windows)
-              complete-counts (apply merge-with + (map (fn [[_ _ window-counts]] window-counts) complete-windows))
-              complete-sorted-counts (sort-by (fn [[k kc]] (- kc)) complete-counts)
-              complete-high-counts (take 10 complete-sorted-counts)
-              complete-rates (map (fn [[k kc]] [k (/ kc 10.0)]) complete-high-counts)]
-          [recent-windows complete-rates]))})
-
-(def events-per-second-by-cloud
-  {:receive-init
-     (fn []
-       [(util/millis) {}])
-   :receive-apply
-     (fn [[window-start window-counts] event]
-       [window-start (update window-counts (str (or (:cloud event) "none")) safe-inc)])
+       [window-start (if (pred-fn event) (update window-counts (str (key-fn event)) safe-inc) window-counts)])
    :receive-emit
      (fn [[window-start window-counts]]
        [window-start (util/millis) window-counts])
@@ -166,13 +82,41 @@
      (fn [last-val]
        [last-val last-val])})
 
+
+(defn heroku? [evt]
+  (= (:cloud evt) "heroku.com"))
+
+(def events-per-second
+  (rate
+    (fn [evt] true)))
+
+(def events-per-second-by-parsed
+  (rate-by-key
+    (fn [evt] true)
+    (fn [evt] (:parsed evt))))
+
+(def events-per-second-by-aorta-host
+  (rate-by-key
+    (fn [evt] true)
+    (fn [evt] (:aorta_host evt))))
+
+(def events-per-second-by-event-type
+  (rate-by-key
+    (fn [evt] true)
+    (fn [evt] (or (:event_type evt) "none"))))
+
+(def events-per-second-by-cloud
+  (rate-by-key
+    (fn [evt] true)
+    (fn [evt] (or (:cloud evt) "none"))))
+
 (def ps-lost-last
   (last
     (fn [evt] (and (heroku? evt) (:process_lost evt)))
     (fn [evt] (:total_count evt))))
 
 (def all
-  [["events_per_second" events]
+  [["events_per_second" events-per-second]
    ["events_per_second_by_parsed" events-per-second-by-parsed]
    ["events_per_second_by_aorta_host" events-per-second-by-aorta-host]
    ["events_per_second_by_event_type" events-per-second-by-event-type]
