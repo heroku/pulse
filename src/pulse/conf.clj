35c03b46b3329c98e3740f067c970aec339f4cdd
Mark McGranaghan <mmcgrana@gmail.com>
2011-06-28 21:04:40 -0700

    require port

--- src/pulse/conf.clj
+++ src/pulse/conf.clj
@@ -2,6 +2,6 @@
   (:require [clojure.string :as str]))
 
 (def env (System/getenv))
-(def port (Integer/parseInt (get env "PORT" "8080")))
+(def port (Integer/parseInt (get env "PORT")))
 (def redis-url (or (get env "REDIS_URL") (get env "REDISTOGO_URL")))
 (def aorta-urls (if-let [h (get env "AORTA_URLS")] (str/split h #",")))
