105716d0710c750ba29a55b851d190d934ed1e62
Mark McGranaghan <mmcgrana@gmail.com>
2011-06-28 21:44:57 -0700

    update deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject pulse "0.0.1"
   :dependencies [
-    [org.clojure/clojure "1.3.0-alpha8"]
+    [org.clojure/clojure "1.3.0-beta1"]
     [clj-redis "0.0.8"]
     [clj-json "0.3.1"]
     [hiccup "0.3.4"]
     [ring-basic-auth "0.1.0"]
-    [ring/ring-devel "0.3.8"]
-    [ring/ring-jetty-async-adapter "0.3.3-SNAPSHOT"]])
+    [ring/ring-devel "0.3.10"]
+    [ring/ring-jetty-adapter "0.3.10"]])
