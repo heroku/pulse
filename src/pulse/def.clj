efadaa7414ca446a02ced87879402e2c4998ea7a
Mark Fine <mark.fine@gmail.com>
2011-10-04 14:29:49 -0700

    More changes around up_to_up and counts.

--- src/pulse/def.clj
+++ src/pulse/def.clj
@@ -333,45 +333,45 @@
 
 (defstat ps-up-total-last
   (last
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:counts evt) (= (:event evt) "emit")))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "counts") (= (:event evt) "emit")))
     (fn [evt] (:up evt))))
 
 (defstat ps-up-web-last
   (last
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:counts evt) (= (:event evt) "emit")))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "counts") (= (:event evt) "emit")))
     (fn [evt] (:web evt))))
 
 (defstat ps-up-worker-last
   (last
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:counts evt) (= (:event evt) "emit")))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "counts") (= (:event evt) "emit")))
     (fn [evt] (:worker evt))))
 
 (defstat ps-up-other-last
   (last
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:counts evt) (= (:event evt) "emit")))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "counts") (= (:event evt) "emit")))
     (fn [evt] (:other evt))))
 
 (defstat ps-created-last
   (last
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:counts evt) (= (:event evt) "emit")))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "counts") (= (:event evt) "emit")))
     (fn [evt] (:created evt))))
 
 (defstat ps-starting-last
   (last
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:counts evt) (= (:event evt) "emit")))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "counts") (= (:event evt) "emit")))
     (fn [evt] (:starting evt))))
 
 (defstat ps-idles-per-minute
   (per-minute
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:up_to_up evt) (= (:event evt) "idle")))))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "up_to_up") (= (:event evt) "idle")))))
 
 (defstat ps-unidles-per-minute
   (per-minute
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:service evt) (:unidle evt) (= (:event evt) "begin")))))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (:service evt) (= (:function evt) "unidle") (= (:event evt) "begin")))))
 
 (defstat ps-crashed-last
   (last
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:counts evt) (= (:event evt) "emit")))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "counts") (= (:event evt) "emit")))
     (fn [evt] (:crashed evt))))
 
 (defstat ps-running-total-last
@@ -418,10 +418,6 @@
   (per-minute
     (fn [evt] (and (heroku? evt) (:ps_watch evt) (:trap_exit evt)))))
 
-(defstat ps-converges-per-second2
-  (per-second
-    (fn [evt] (and (heroku? evt) (:service evt) (:transition evt)))))
-
 (defstat ps-converges-per-second
   (per-second
     (fn [evt] (and (heroku? evt) (:service evt) (= (:function evt) "transition")))))
@@ -437,7 +433,7 @@
 
 (defstat ps-lost-last
   (last
-    (fn [evt] (and (heroku? evt) (:psmgr evt) (:counts evt) (= (:event evt) "emit")))
+    (fn [evt] (and (heroku? evt) (:psmgr evt) (= (:function evt) "counts") (= (:event evt) "emit")))
     (fn [evt] (:lost evt))))
 
 (defn errors-per-minute [component]
