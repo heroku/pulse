37199c265d279f5bb4bb212445ef7c5c5b03b267
Mark McGranaghan <mmcgrana@gmail.com>
2011-11-25 15:26:45 -0800

    allow colons

--- src/pulse/parse.clj
+++ src/pulse/parse.clj
@@ -46,7 +46,7 @@
   (if s (Long/parseLong s)))
 
 (def standard-re
-  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?[\-\+]\d\d:00) [0-9\.]+ [a-z0-7]+\.([a-z]+) ([a-z\-\_]+)(\[(\d+)\])? - ([a-z4-6\-\_]+)?\.(\d+)@([a-z.\-]+\.com) - (.*)$")
+  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d(\.\d+)?[\-\+]\d\d:00) [0-9\.]+ [a-z0-7]+\.([a-z]+) ([a-z\-\_]+)(\[(\d+)\])?:? - ([a-z4-6\-\_]+)?\.(\d+)@([a-z.\-]+\.com) - (.*)$")
 
 (defn parse-standard-line [l]
   (let [m (re-matcher standard-re l)]
