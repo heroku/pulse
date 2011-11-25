0e9d7e4260da4873e35deab68b163a7338836fbc
Mark McGranaghan <mmcgrana@gmail.com>
2011-11-25 14:00:35 -0800

    do not parse out host or facility - these are implementation details that should not propagate to receivers

--- src/pulse/parse.clj
+++ src/pulse/parse.clj
@@ -51,7 +51,7 @@
   (if s (Long/parseLong s)))
 
 (def standard-re
-  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) ([0-9\.]+) ([a-z0-7]+)\.([a-z]+) ([a-z\-\_]+)(\[(\d+)\])? - ([a-z4-6-]+)?\.(\d+)@([a-z.]+\.com) - (.*)$")
+  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) [0-9\.]+ [a-z0-7]+\.([a-z]+) ([a-z\-\_]+)(\[(\d+)\])? - ([a-z4-6-]+)?\.(\d+)@([a-z.]+\.com) - (.*)$")
 
 (defn parse-standard-line [l]
   (let [m (re-matcher standard-re l)]
@@ -59,89 +59,79 @@
       (merge
         {:event_type "standard"
          :timestamp_src (parse-timestamp (.group m 1))
-         :host (.group m 2)
-         :facility (.group m 3)
-         :level (.group m 4)
-         :component (.group m 5)
-         :pid (parse-long (.group m 7))
-         :slot (.group m 8)
-         :instance_id (Long/parseLong (.group m 9))
-         :cloud (.group m 10)}
-        (parse-message-attrs (.group m 11))))))
+         :level (.group m 2)
+         :component (.group m 3)
+         :pid (parse-long (.group m 5))
+         :slot (.group m 6)
+         :instance_id (Long/parseLong (.group m 7))
+         :cloud (.group m 8)}
+        (parse-message-attrs (.group m 9))))))
 
 (def raw-re
-  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) ([0-9\.]+) ([a-z0-7]+)\.([a-z]+) (.*)$")
+  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) [0-9\.]+ [a-z0-7]+\.([a-z]+) (.*)$")
 
 (defn parse-raw-line [l]
   (let [m (re-matcher raw-re l)]
     (if (.find m)
       {:event_type "raw"
        :timestamp_src (parse-timestamp (.group m 1))
-       :host (.group m 2)
-       :facility (.group m 3)
-       :level (.group m 4)
-       :message (.group m 5)})))
+       :level (.group m 2)
+       :message (.group m 3)})))
 
 (def nginx-access-re
-     ;timestamp_src                                   ;host      ;facility    ;level           ;slot        ;ins_id ;cloud           ;http_host                                                              ;http_method,_url,_version      ;http_status,_bytes,_referrer,_user_agent,_domain
-  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) ([0-9\.]+) ([a-z0-7]+)\.([a-z]+) nginx - ([a-z4-6-]+)?\.(\d+)@([a-z.]+\.com) - ([0-9\.]+) - - \[\d\d\/[a-zA-z]{3}\/\d\d\d\d:\d\d:\d\d:\d\d -\d\d00\] \"([a-zA-Z]+) (\S+) HTTP\/(...)\" (\d+) (\d+) \"([^\"]+)\" \"([^\"]+)\" (\S+)$")
+     ;timestamp_src                                   ;host    ;facility  ;level           ;slot        ;ins_id ;cloud           ;http_host                                                              ;http_method,_url,_version      ;http_status,_bytes,_referrer,_user_agent,_domain
+  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) [0-9\.]+ [a-z0-7]+\.([a-z]+) nginx - ([a-z4-6-]+)?\.(\d+)@([a-z.]+\.com) - ([0-9\.]+) - - \[\d\d\/[a-zA-z]{3}\/\d\d\d\d:\d\d:\d\d:\d\d -\d\d00\] \"([a-zA-Z]+) (\S+) HTTP\/(...)\" (\d+) (\d+) \"([^\"]+)\" \"([^\"]+)\" (\S+)$")
 
 (defn parse-nginx-access-line [l]
   (let [m (re-matcher nginx-access-re l)]
     (if (.find m)
        {:event_type "nginx_access"
         :timestamp_src (parse-timestamp (.group m 1))
-        :host (.group m 2)
-        :facility (.group m 3)
-        :level (.group m 4)
+        :level (.group m 2)
         :component "nginx"
-        :slot (.group m 5)
-        :instance_id (Long/parseLong (.group m 6))
-        :cloud (.group m 7)
-        :http_host (.group m 8)
-        :http_method (.group m 9)
-        :http_url (.group m 10)
-        :http_version (.group m 11)
-        :http_status (Long/parseLong (.group m 12))
-        :http_bytes (Long/parseLong (.group m 13))
-        :http_referrer (.group m 14)
-        :http_user_agent (.group m 15)
-        :http_domain (.group m 16)})))
+        :slot (.group m 3)
+        :instance_id (Long/parseLong (.group m 4))
+        :cloud (.group m 5)
+        :http_host (.group m 6)
+        :http_method (.group m 7)
+        :http_url (.group m 8)
+        :http_version (.group m 9)
+        :http_status (Long/parseLong (.group m 10))
+        :http_bytes (Long/parseLong (.group m 11))
+        :http_referrer (.group m 12)
+        :http_user_agent (.group m 13)
+        :http_domain (.group m 14)})))
 
 (def nginx-error-re
-  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) ([0-9\.]+) ([a-z0-7]+)\.([a-z]+) nginx - ([a-z4-6]+)?\.(\d+)@([a-z.]+\.com) - .* \[error\] (.*)$")
+  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-\+]\d\d:00) [0-9\.]+ [a-z0-7]+\.([a-z]+) nginx - ([a-z4-6]+)?\.(\d+)@([a-z.]+\.com) - .* \[error\] (.*)$")
 
 (defn parse-nginx-error-line [l]
   (let [m (re-matcher nginx-error-re l)]
     (if (.find m)
        {:event_type "nginx_error"
         :timestamp_src (parse-timestamp (.group m 1))
-        :host (.group m 2)
-        :facility (.group m 3)
-        :level (.group m 4)
+        :level (.group m 2)
         :component "nginx"
-        :slot (.group m 5)
-        :instance_id (Long/parseLong (.group m 6))
-        :cloud (.group m 7)
-        :message (.group m 8)})))
+        :slot (.group m 3)
+        :instance_id (Long/parseLong (.group m 4))
+        :cloud (.group m 5)
+        :message (.group m 6)})))
 
 (def varnish-access-re
-  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-+]\d\d:00) ([0-9\.]+) ([a-z0-7]+)\.([a-z]+) varnish\[(\d+)\] - ([a-z4-6\-]+)?\.(\d+)@([a-z.]+\.com) - [0-9\.]+ - - .*\" (\d\d\d) .*$")
+  #"^(\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d[\-+]\d\d:00) [0-9\.]+ [a-z0-7]+\.([a-z]+) varnish\[(\d+)\] - ([a-z4-6\-]+)?\.(\d+)@([a-z.]+\.com) - [0-9\.]+ - - .*\" (\d\d\d) .*$")
 
 (defn parse-varnish-access-line [l]
   (let [m (re-matcher varnish-access-re l)]
     (if (.find m)
        {:event_type "varnish_access"
         :timestamp_src (parse-timestamp (.group m 1))
-        :host (.group m 2)
-        :facility (.group m 3)
-        :level (.group m 4)
+        :level (.group m 2)
         :component "varnish"
-        :pid (parse-long (.group m 5))
-        :slot (.group m 6)
-        :instance_id (parse-long (.group m 7))
-        :cloud (.group m 8)
-        :http_status (parse-long (.group m 9))})))
+        :pid (parse-long (.group m 3))
+        :slot (.group m 4)
+        :instance_id (parse-long (.group m 5))
+        :cloud (.group m 6)
+        :http_status (parse-long (.group m 7))})))
 
 (defn log [& data]
   (apply log/log :ns "parse" data))
