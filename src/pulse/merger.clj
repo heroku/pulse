983a87399587d8476d4b5694371f52e50dc002ef
Mark McGranaghan <mmcgrana@gmail.com>
2011-11-26 11:50:06 -0800

    consistent naming

--- src/pulse/merger.clj
+++ src/pulse/merger.clj
@@ -40,11 +40,11 @@
   (log :fn "main" :at "start")
   (let [apply-queue (queue/init 2000)
         publish-queue (queue/init 100)
-        stats-states (init-stats def/all)]
+        stats-map (init-stats def/all)]
     (queue/init-watcher apply-queue "apply")
     (queue/init-watcher publish-queue "publish")
     (io/init-publishers publish-queue (conf/redis-url) "stats.merged" json/generate-string (conf/publish-threads))
-    (init-emitter stats-states publish-queue)
-    (init-appliers stats-states apply-queue (conf/apply-threads))
+    (init-emitter stats-map publish-queue)
+    (init-appliers stats-map apply-queue (conf/apply-threads))
     (io/init-subscriber (conf/redis-url) "stats.received" read-string apply-queue))
   (log :fn "main" :at "finish"))
