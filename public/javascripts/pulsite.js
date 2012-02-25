1ef8f8eb64627ff769b807b95c0b02ceff8c3d07
obfuscurity <jdixon@heroku.com>
2012-02-25 11:59:56 -0500

    determine our edge location and compensate

--- public/javascripts/pulsite.js
+++ public/javascripts/pulsite.js
@@ -2,14 +2,44 @@ $(document).ready(function() {
   var id;
   var metric;
   var timer;
+  var width = 800;
+  var height = 400;
+  var margin = 30;
+
+  // calculate graph location
+  function setPosition(coords) {
+    this.up: 0,
+    this.right: function() {
+      if (coords.left + width > availWidth) {
+        return availWidth - (coords.left + width);
+      } else {
+        return -50;
+      }
+    }
+    this.left: 0,
+    this.down: function() {
+      if (coords.top + height > availHeight) {
+        return availHeight - (coords.top + height);
+      } else {
+        return -50;
+      }
+    }
+  }
+
+  // insert graph
   function loadGraph() {
-    var period = graphitePeriod || 3600;
-    var options = 'width=800&height=400&margin=30&areaMode=all&fontSize=14&from=-' + period + 'seconds';
-    var url = graphiteApiUrl + '/render/?' + options + '&target=pulse.';
-    var style = 'style="position: absolute; margin: -50px 0 0 -150px;"';
+    var shape = = 'width=' + width + '&height=' + height + '&margin=' + margin;
+    var options = '&areaMode=all&fontSize=14';
+    var period = '&from=-' + (graphitePeriod || 3600) + 'seconds';
+    var url = graphiteApiUrl + '/render/?' + shape + options + period + '&target=pulse.';
+    var pos = new setPosition($('#' + id).position());
+    var coords = pos.up + ' ' + pos.right + ' ' + pos.down + ' ' + pos.left;
+    var style = 'style="position: absolute; margin: ' coords + ';"';
     $('#' + id).before('<img id="hoverGraph" src="' + url + metric + '" ' + style + '>');
     console.log("opening " + url + metric);
   }
+
+  // main
   $('td').hover(function() {
     id = $(this).find('span').attr('id');
     metric = id.replace(/-sparkline$/, '');
