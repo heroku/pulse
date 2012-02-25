41ac208c45d08785c217c2f5e9882d1755ae2228
obfuscurity <jdixon@heroku.com>
2012-02-25 18:30:06 -0500

    fixes to edge determination, remove some console output

--- public/javascripts/pulsite.js
+++ public/javascripts/pulsite.js
@@ -7,36 +7,24 @@ $(document).ready(function() {
   var margin = 30;
 
   // calculate graph location
-  function setPosition(coords) {
-    this.up: 0,
-    this.right: function() {
-      if (coords.left + width > availWidth) {
-        return availWidth - (coords.left + width);
-      } else {
-        return -50;
-      }
-    }
-    this.left: 0,
-    this.down: function() {
-      if (coords.top + height > availHeight) {
-        return availHeight - (coords.top + height);
-      } else {
-        return -50;
-      }
-    }
+  function setPosition(loc) {
+    this.up = 0;
+    this.right = (loc.left + width > screen.availWidth) ? screen.availWidth - (loc.left + width) : -50;
+    this.left = 0;
+    this.down = (loc.top + height > screen.availHeight) ? screen.availHeight - (loc.top + height) : -50;
   }
 
   // insert graph
   function loadGraph() {
-    var shape = = 'width=' + width + '&height=' + height + '&margin=' + margin;
+    var shape = 'width=' + width + '&height=' + height + '&margin=' + margin;
     var options = '&areaMode=all&fontSize=14';
     var period = '&from=-' + (graphitePeriod || 3600) + 'seconds';
     var url = graphiteApiUrl + '/render/?' + shape + options + period + '&target=pulse.';
     var pos = new setPosition($('#' + id).position());
     var coords = pos.up + ' ' + pos.right + ' ' + pos.down + ' ' + pos.left;
-    var style = 'style="position: absolute; margin: ' coords + ';"';
+    console.log('coords: ' + coords);
+    var style = 'style="position: absolute; margin: ' + coords + ';"';
     $('#' + id).before('<img id="hoverGraph" src="' + url + metric + '" ' + style + '>');
-    console.log("opening " + url + metric);
   }
 
   // main
@@ -44,10 +32,8 @@ $(document).ready(function() {
     id = $(this).find('span').attr('id');
     metric = id.replace(/-sparkline$/, '');
     timer = setTimeout(loadGraph, 500);
-    console.log(id, metric, timer);
   }, function() {
     $('#hoverGraph').remove();
     clearTimeout(timer);
-    console.log("killing " + timer + ", closing window");
   });
 });
