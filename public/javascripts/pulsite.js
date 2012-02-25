b655cc93f7913e582762b964a19acd9a71582b67
obfuscurity <jdixon@heroku.com>
2012-02-25 00:03:41 -0500

    import of pulsite

---
+++ public/javascripts/pulsite.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  var id;
+  var metric;
+  var timer;
+  var options = 'width=800&height=400&margin=30&areaMode=all&fontSize=14&from=-1hours';
+  var url = graphiteApiUrl + '/render/?' + options + '&target=pulse.';
+  function loadGraph() {
+    var style = 'style="position: absolute; margin: -50px 0 0 -150px;"';
+    $('#' + id).before('<img id="hoverGraph" src="' + url + metric + '" ' + style + '>');
+    console.log("opening " + url + metric);
+  }
+  $('td').hover(function() {
+    id = $(this).find('span').attr('id');
+    metric = id.replace(/-sparkline$/, '');
+    timer = setTimeout(loadGraph, 500);
+    console.log(id, metric, timer);
+  }, function() {
+    $('#hoverGraph').remove();
+    clearTimeout(timer);
+    console.log("killing " + timer + ", closing window");
+  });
+});
