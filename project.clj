0d3943a5e1e9a4b7fbbd06cd8f12727d96a7be00
Phil Hagelberg <technomancy@gmail.com>
2012-03-15 20:11:03 +0000

    Tell swank to skip redirecting *out*'s root to the repl.

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
                  [org.jboss.netty/netty "3.2.7.Final"]
                  [hiccup "0.3.8"]
                  [ring-basic-auth "0.1.0"]
-                 [ring/ring-jetty-adapter "1.0.1"]])
+                 [ring/ring-jetty-adapter "1.0.1"]]
+  ;; don't want all that logging going to the repl
+  :swank-options {:repl-out-root false})
